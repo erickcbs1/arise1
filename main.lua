42ac998314b015f80351a3e336e0d8d7bb47798c
erickcbs1 <erickts4437@gmail.com>
2025-04-21 22:35:21 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,13 @@
 
 -- Arquivo principal que carrega a interface e scripts
 print("Iniciando...")
-local funcs = loadstring(game:HttpGet("https://raw.githubusercontent.com/SEU_USUARIO/arise/main/src/scripts/funcs.lua"))()
-local gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/SEU_USUARIO/arise/main/src/ui/gui.lua"))()
+local HttpService = game:GetService("HttpService")
+
+local funcsCode = HttpService:GetAsync("https://raw.githubusercontent.com/erickcbs1/arise/main/src/scripts/funcs.lua") 
+local guiCode = HttpService:GetAsync("https://raw.githubusercontent.com/erickcbs1/arise/main/src/ui/gui.lua")
+
+local funcs = loadstring(funcsCode)()
+local gui = loadstring(guiCode)()
+
 gui:Setup(funcs)
 print("Injetado Com Sucesso")
