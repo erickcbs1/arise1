526b92935420266cdd7f1deaad597f10e3cad696
erickcbs1 <erickts4437@gmail.com>
2025-04-21 22:34:09 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 
 -- Arquivo principal que carrega a interface e scripts
 print("Iniciando...")
-loadstring(readfile("src/ui/gui.lua"))()
-loadstring(readfile("src/scripts/funcs.lua"))()
+local funcs = loadstring(game:HttpGet("https://raw.githubusercontent.com/SEU_USUARIO/arise/main/src/scripts/funcs.lua"))()
+local gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/SEU_USUARIO/arise/main/src/ui/gui.lua"))()
+gui:Setup(funcs)
 print("Injetado Com Sucesso")
